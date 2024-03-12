057f5d06bcc7f6c6bed4728678f50a89910597d1
dalan <dalan_123@126.com>
2024-03-12 16:24:06 +0800

    update

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,11 +5,10 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'s> {
+    author: &'s str,
+    title: &'s str,
 }
 
 fn main() {
